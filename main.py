162a2debc68908d0677a39e862fa10844b2c2810
lukalelovic <lukalelovic@gmail.com>
2022-12-19 17:55:35 -0600

    remove redundant lines, clean up comments

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from sklearn.metrics import mean_squared_error
 df = pd.read_csv('all_stocks_5yr.csv')
 
 # Clean and organize the data
-df.dropna(inplace=True) # Remove missing values
-df = df.drop('Name', axis=1)
+df.dropna(inplace=True) # Remove missing value row/cols (if any)
 df['date'] = pd.to_datetime(df['date']) 
 df.set_index('date', inplace=True)
 
@@ -17,20 +16,22 @@ df['day_of_week'] = df.index.dayofweek
 df['month'] = df.index.month
 df['year'] = df.index.year-2000
 
-X = df[['day_of_week', 'month', 'year', 'open']].values
-Y = df['close']
+X = df[['day_of_week', 'month', 'year', 'open']].values # Prediction values
+Y = df['close'] # Value to predict
 
 # Split the data into a training set and a test set
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-# Choose a machine learning model
+# Initialize model to linear regression
 model = LinearRegression()
 
-# Train the model on the training data
+# Train the model on training data
 model.fit(X_train, Y_train)
 
-# Evaluate the model on the test data
+# Apply predictions on test data
 Y_pred = model.predict(X_test)
+
+# Calculate model performance
 r2 = model.score(X_test, Y_test)
 mse = mean_squared_error(Y_test, Y_pred)
 
@@ -39,7 +40,7 @@ print(f'Coefficient of determination (R^2): {r2:.2f}')
 print(f'Mean squared error: {mse:.2f}')
 print()
 
-# Make predictions on new data
+# Prompt for input values
 print('Enter day of week (0=Mond, 1=Tues, etc)')
 day_of_week = int(input())
 
@@ -52,6 +53,5 @@ year = int(input())-2000
 print('Enter opening market price')
 open_price = float(input())
 
-new_data = [ [day_of_week, month, year, open_price] ]
-prediction = model.predict(new_data)
-print(f'Predicted Closing Stock Value: {float(prediction):.2f}')
+closing_value = model.predict([ [day_of_week, month, year, open_price] ])
+print(f'Predicted Closing Stock Value: {float(closing_value):.2f}')
